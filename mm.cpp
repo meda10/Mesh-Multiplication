1180fb57e7b425a673044e22f36eea0903d8ccae
meda10 <32466683+meda10@users.noreply.github.com>
2021-04-23 21:14:40 +0200

    algorithm

--- mm.cpp
+++ mm.cpp
@@ -11,22 +11,7 @@
 const char * FILENAME_MATRIX_1 = "mat1";
 const char * FILENAME_MATRIX_2 = "mat2";
 #define TAG 0
-
-//
-//void matRead(fstream &fin, vector<vector<int> > &mat, int *rows, int *columns) {
-//    string line;
-//    getline(fin, line);
-//
-//    while(getline(fin, line)) {
-//        if(line.size() == 0) break; // extra newline at the end of file
-//        istringstream is(line);
-//        mat.push_back(vector<int>(istream_iterator<int>(is), istream_iterator<int>()));
-//    }
-//
-//    *rows = mat.size();
-//    *columns = mat[0].size();
-//}
-
+#define NO_NEIGHBOUR -1
 
 void print_matrix(const std::vector<std::vector<int>>& matrix){
     for (const auto& line: matrix) {
@@ -45,7 +30,7 @@ std::vector<std::vector<int>> read_input(const char* filename, int *size){
 
     file.open(filename, std::ios::in);
     if (!file.is_open()){
-        MPI_Finalize();
+        MPI_Abort(MPI_COMM_WORLD, 1);
         exit(1);
     }
 
@@ -78,9 +63,9 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD,&my_id);
 
     MPI_Status stat;
-    int prod = 0;       // Resulting dot product for the field controled byt this process.
-    int matrix_rows_1, matrix_rows_2, matrix_columns_1, matrix_columns_2;
-//           M                N             N                 K
+
+    int matrix_rows_1, matrix_columns_1, matrix_rows_2, matrix_columns_2;
+//           M                N               N                K
 
     if(my_id == 0) {
         std::vector<std::vector<int>> matrix_1 = read_input(FILENAME_MATRIX_1, &matrix_rows_1);
@@ -90,108 +75,90 @@ int main(int argc, char *argv[]) {
         matrix_rows_2 = matrix_2.size();
 
         if(matrix_columns_1 != matrix_rows_2){
-            MPI_Finalize();
+            MPI_Abort(MPI_COMM_WORLD, 1);
             exit(1);
         }
 
-//        // DEBUG - time measurement
-//        // double tFrom_wtime = MPI::Wtime();
-//
-//        // Send the number of columns of matrix 1 to each process
-//        for(int i = 1; i < numprocs; ++i) {
-//            MPI_Send(&m, 1, MPI_INT, i, TAG, MPI_COMM_WORLD);
-//            MPI_Send(&n, 1, MPI_INT, i, TAG, MPI_COMM_WORLD);
-//            MPI_Send(&k, 1, MPI_INT, i, TAG, MPI_COMM_WORLD);
-//        }
-//
-//        // Mesh multiplication algorithm
-//        for(int i = 0; i < max(m + n - 1, n + k - 1); ++i) {
-//            // Send values from matrix 1 to all processes.
-//            if(i < m + n - 1) {
-//                int c = max(0, n - 1 - i);
-//                int r = i - n + c + 1;
-//
-//                while(r < m && c < n) {
-//                    if(r > 0) {
-//                        int tmp = mat1[r][c];
-//                        MPI_Send(&tmp, 1, MPI_INT, r * k, TAG, MPI_COMM_WORLD);
-//                    }
-//                    r += 1;
-//                    c += 1;
-//                }
-//            }
-//
-//
-//            // Send values from matrix 2 to all processes.
-//            if(i < n + k - 1) {
-//                int r = max(0, n - 1 - i);
-//                int c = i - n + r + 1;
-//
-//                while(r < n && c < k) {
-//                    if(c > 0) {
-//                        int tmp = mat2[r][c];
-//                        MPI_Send(&tmp, 1, MPI_INT, c, TAG, MPI_COMM_WORLD);
-//                    }
-//                    r += 1;
-//                    c += 1;
-//                }
-//            }
-//
-//            // compute product and send values L -> R, T -> B
-//            if(i < n) {
-//                int left = mat1[0][n - 1 - i];
-//                int top  = mat2[n - 1 - i][0];
-//                prod += left * top;
-//
-//                // Send current values from left and top vector to right and bottom processes.
-//                if(k > 1) MPI_Send(&left, 1, MPI_INT, 1, TAG, MPI_COMM_WORLD); // LEFT -> RIGHT
-//                if(m > 1) MPI_Send(&top,  1, MPI_INT, k, TAG, MPI_COMM_WORLD); // TOP  -> DOWN
-//            }
-//        }
-//
-//        // Read resulting values from all processes and print out
-//        cout << m << ":" << k << endl;
-//        cout << prod;
-//        for(int proci = 1; proci < numprocs; ++proci) {
-//            int tmp;
-//            MPI_Recv(&tmp, 1, MPI_INT, proci, TAG, MPI_COMM_WORLD, &stat);
-//            cout << ((proci % mat2c == 0) ? "\n" : " ") << tmp;
-//        }
-//        cout << endl;
-//
-//
-//        // DEBUG - time measurement
-//        // double tTo_wtime = MPI::Wtime();
-//        // cout << 1000.0 * (tTo_wtime - tFrom_wtime) << endl;
-    }
-//
-//    /* --- All CPUs except the first one --- */
-//    if(myid > 0) {
-//        // Receive matrices dimensions frmo process 0.
-//        MPI_Recv(&m, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, &stat);
-//        MPI_Recv(&n, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, &stat);
-//        MPI_Recv(&k, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, &stat);
-//
-//        // Iteratively compute the dot product of the top and vector left vector, send values to right and bottom.
-//        int left, top;
-//        for(int i = 0; i < n; ++i) {
-//            // CPU [1][0]
-//            if((m > 1) && (myid == k)) {
-//                MPI_Recv(&top,  1, MPI_INT, 0, TAG, MPI_COMM_WORLD, &stat);
-//                MPI_Recv(&left, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, &stat);
-//            } else {
-//                MPI_Recv(&left, 1, MPI_INT, (myid == 1 || myid % k == 0) ? 0 : myid - 1, TAG, MPI_COMM_WORLD, &stat);
-//                MPI_Recv(&top,  1, MPI_INT, (myid <= k) ? 0 : myid - k,                  TAG, MPI_COMM_WORLD, &stat);
-//            }
-//            prod += left * top;
-//
-//            if((myid + 1) % k != 0)   MPI_Send(&left, 1, MPI_INT, myid + 1, TAG, MPI_COMM_WORLD);
-//            if(myid < (numprocs - k)) MPI_Send(&top,  1, MPI_INT, myid + k, TAG, MPI_COMM_WORLD);
-//        }
-//        MPI_Send(&prod, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD);
-//    }
+        MPI_Bcast(&matrix_rows_1, 1, MPI_INT, TAG, MPI_COMM_WORLD);
+        MPI_Bcast(&matrix_columns_1, 1, MPI_INT, TAG, MPI_COMM_WORLD);
+        MPI_Bcast(&matrix_columns_2, 1, MPI_INT, TAG, MPI_COMM_WORLD);
 
-    // Finalize MPI
+
+        //Sends
+        for(int j = 0; j < matrix_columns_1; j++){
+            for(int i = 0; i < matrix_rows_1; i++){
+//                std::cout << matrix_1[i][j] << ' ';
+                MPI_Send(&matrix_1[i][j], 1, MPI_INT, j * matrix_columns_2, TAG, MPI_COMM_WORLD);
+            }
+        }
+
+        for(int i = 0; i < matrix_rows_2; i++){
+            for(int j = 0; j < matrix_columns_2; j++){
+//                std::cout << matrix_2[i][j] << ' ';
+                MPI_Send(&matrix_2[i][j], 1, MPI_INT, j, TAG, MPI_COMM_WORLD);
+            }
+        }
+
+        int sum = 0;
+        for(int i = 0; i < matrix_columns_1; i++){ //2
+            int a = matrix_1[0][matrix_columns_1 - 1 - i];
+            int b = matrix_2[matrix_rows_2 - 1 - i][0];
+            sum += a * b;
+
+            // Send current values from left and top vector to right and bottom processes.
+            if(matrix_columns_2 > 1) {
+                std::cout << "SEND " << b << " from " << 0 << " sending to " << 1 << std::endl;
+                MPI_Send(&a, 1, MPI_INT, 1, TAG, MPI_COMM_WORLD); // A -> RIGHT
+            }
+            if(matrix_rows_1 > 1){
+                std::cout << "SEND " << b << " from " << 0 << " sending to " << matrix_columns_2 << std::endl;
+                MPI_Send(&b, 1, MPI_INT, matrix_columns_2, TAG, MPI_COMM_WORLD); // B  -> DOWN
+            }
+        }
+//
+        // Read resulting values from all processes and print out
+        std::cout << matrix_rows_1 << ":" << matrix_columns_2 << std::endl;
+        std::cout << sum;
+        for(int proci = 1; proci < number_of_processors; ++proci) {
+            int tmp;
+            MPI_Recv(&tmp, 1, MPI_INT, proci, TAG, MPI_COMM_WORLD, &stat);
+            std::cout << ((proci % matrix_columns_2 == 0) ? "\n" : " ") << tmp;
+        }
+        std::cout << std::endl;
+
+    }
+
+    if(my_id > 0) {
+        MPI_Recv(&matrix_rows_1, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, &stat);
+        MPI_Recv(&matrix_columns_1, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, &stat);
+        MPI_Recv(&matrix_columns_2, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, &stat);
+
+        // Get IDs of processor neighbours
+        int top = my_id - matrix_columns_2 < 0 ? NO_NEIGHBOUR : my_id - matrix_columns_2;
+        int bottom = my_id + matrix_columns_2 > number_of_processors ? NO_NEIGHBOUR : my_id + matrix_columns_2;
+        int left = my_id % matrix_columns_2 == 0 ? NO_NEIGHBOUR : my_id - 1;
+        int right = (my_id + 1 ) % matrix_columns_2 == 0 ? NO_NEIGHBOUR : my_id + 1;
+
+        int a, b;
+        int sum = 0;
+        for(int i = 0; i < matrix_columns_1; ++i) {
+            MPI_Recv(&a, 1, MPI_INT, left, TAG, MPI_COMM_WORLD, &stat);
+            MPI_Recv(&b,  1, MPI_INT, top, TAG, MPI_COMM_WORLD, &stat);
+
+            std::cout << "RECV " << a << " from " << left << " sending to " << right << std::endl;
+            std::cout << "RECV " << b << " from " << top << " sending to " << bottom << std::endl;
+
+            sum += a*b;
+            if(right != NO_NEIGHBOUR){
+                MPI_Send(&a, 1, MPI_INT, right, TAG, MPI_COMM_WORLD);
+            }
+            if(bottom != NO_NEIGHBOUR){
+                MPI_Send(&b,  1, MPI_INT, bottom, TAG, MPI_COMM_WORLD);
+            }
+        }
+        MPI_Send(&sum, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD);
+    }
+
     MPI_Finalize();
     return 0;
 }
